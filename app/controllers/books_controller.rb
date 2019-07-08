7eb3e4e3a7124f55554420161d268bd36d983914
Eric Tenza <ejtenza@gmail.com>
2019-07-08 02:50:06 -0500

    Remove unnecessary logging from books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,6 @@ class BooksController < ApplicationController
     @page = query_params[:page].to_i
     @query = query_params[:query]
     @books = Rails.cache.fetch([@query, @page]) do
-      Rails.logger.info "Making an API call for '#{@query}', page #{@page}"
       parsed_response = HttpClient.new(GoogleBooksStrategy.new(query_params)).call
       BookCollection.new(parsed_response).assemble
     end
